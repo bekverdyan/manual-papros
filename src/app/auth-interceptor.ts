08032181782dbd3ada9e21f425cfbc002fd865af
bekverdyan <bekor@bk.ru>
2019-06-20 01:48:01 +0400

    not fully tested http interceptor implemented

---
+++ src/app/auth-interceptor.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { AuthService } from './auth.service';
+
+@Injectable()
+export class AuthInterceptor implements HttpInterceptor {
+
+  constructor(public auth: AuthService) { }
+
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+
+    const token = this.auth.token.token;
+
+    if (token) {
+      const cloned = req.clone({
+        headers: req.headers.set('Authorization', token)
+      });
+
+      return next.handle(cloned);
+    } else {
+      return next.handle(req);
+    }
+  }
+}
